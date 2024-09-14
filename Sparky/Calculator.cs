29866c8e0fb3e5f99f285421d57944f608564f96
Oswaldo <oscalderon85@gmail.com>
2024-09-13 23:24:56 -0600

    feat(agregado nuevo proyecto Sparky NUnit y probada la funcionalidad de comprobación de número par)

--- Sparky/Calculator.cs
+++ Sparky/Calculator.cs
@@ -7,5 +7,9 @@
             return a + b;
         }
 
+        public bool IsOddNumber(int a)
+        {
+            return a % 2 != 0;
+        }
     }
 }
---
+++ SparkyNUnitTest/CalculatorNUnitTests.cs
@@ -0,0 +1,46 @@
+﻿using Sparky;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SparkyNUnitTest
+{
+    [TestFixture]
+    public class CalculatorNUnitTests
+    {
+        [Test]
+        public void AddNumbers_InputTowInt_GetCorrectAddition()
+        {
+            // Arrange
+            var cal = new Calculator();
+
+            //Act
+            int result = cal.AddNumbers(10,20);
+
+            //Assert
+            Assert.That(result,Is.EqualTo(30));
+        }
+
+        [Test]
+        public void IsOddChecker_InputEvenNumber_ReturnFalse()
+        {
+            var calc = new Calculator();
+
+            bool isOdd = calc.IsOddNumber(10);
+
+            Assert.IsFalse(isOdd);       
+        }
+
+        [Test]
+        public void IsOddChecker_InputOffNumber_ReturnTrue()
+        {
+            var calc = new Calculator();
+
+            bool isOdd = calc.IsOddNumber(11);
+
+            Assert.IsTrue(isOdd);
+        }
+    }
+}
